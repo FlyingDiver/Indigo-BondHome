1b0be5d249a549ad0b3227bf63cc701da9152c88
Joe Keenan <joe@flyingdiver.com>
2019-12-08 14:18:17 -0500

    Generic device only

--- BondHome.indigoPlugin/Contents/Server Plugin/plugin.py
+++ BondHome.indigoPlugin/Contents/Server Plugin/plugin.py
@@ -3,7 +3,6 @@
 
 import json
 import logging
-import time
 import requests
 
 class Plugin(indigo.PluginBase):
@@ -72,17 +71,14 @@ class Plugin(indigo.PluginBase):
             self.logger.threaddebug(json.dumps(self.bridge_data, sort_keys=True, indent=4, separators=(',', ': ')))
                     
                         
-        elif dev.deviceTypeId == "bondShade":
+        elif dev.deviceTypeId == "bondDevice":
             pass
             
-        elif dev.deviceTypeId == "bondFan":
-            pass
-
-        elif dev.deviceTypeId == "bondFireplace":
-            pass
-
         else:
             self.logger.error(u"{}: deviceStartComm: Unknown device type: {}".format(dev.name, dev.deviceTypeId))
+
+        dev.stateListOrDisplayStateIdChanged()
+
             
     def deviceStopComm(self, dev):
         self.logger.info(u"{}: Stopping {} Device {}".format( dev.name, dev.deviceTypeId, dev.id))
@@ -112,32 +108,15 @@ class Plugin(indigo.PluginBase):
             
         bridge_info = self.bridge_data[int(bridge)]
         for dev_key, dev_info in bridge_info.iteritems():
-            self.logger.threaddebug("get_device_list: dev_key = {}, dev_info = {}".format(dev_key, dev_info))
-            if dev_info["type"] == filter:
-                self.logger.debug(u"get_bridge_list adding: {} ({})".format(dev_info["name"], dev_key))         
-                retList.append((dev_key, dev_info["name"]))
+            self.logger.threaddebug("get_device_list: adding dev_key = {}, dev_info = {}".format(dev_key, dev_info))
+            retList.append((dev_key, dev_info["name"]))
         retList.sort(key=lambda tup: tup[1])
         return retList
 
+    #######################################
+
     def get_action_list(self, filter="", valuesDict=None, typeId="", targetId=0):
         self.logger.threaddebug("get_action_list: typeId = {}, targetId = {}, filter = {}, valuesDict = {}".format(typeId, targetId, filter, valuesDict))
-        retList = []
-        bridge = valuesDict.get("bridge", None)
-        self.logger.threaddebug("get_action_list: bridge = {}".format(bridge))
-        if not bridge:
-            return retList
-        address = valuesDict.get("address", None)
-        self.logger.threaddebug("get_action_list: address = {}".format(address))
-        if not address:
-            return retList            
-        action_list = self.bridge_data[int(bridge)][address]['actions']
-        self.logger.debug("get_action_list: action_list = {}".format(action_list))
-        for cmd in action_list:
-            retList.append((cmd, cmd))
-        return retList
-
-    def get_action_list_by_device(self, filter="", valuesDict=None, typeId="", targetId=0):
-        self.logger.threaddebug("get_action_list_by_device: typeId = {}, targetId = {}, filter = {}, valuesDict = {}".format(typeId, targetId, filter, valuesDict))
         retList = []
         dev = indigo.devices[targetId]
         address = dev.address
@@ -145,7 +124,7 @@ class Plugin(indigo.PluginBase):
         if not bridge:
             return retList
         action_list = self.bridge_data[int(bridge)][address]['actions']
-        self.logger.debug("get_action_list_by_device: action_list = {}".format(action_list))
+        self.logger.debug("get_action_list: action_list = {}".format(action_list))
         for cmd in action_list:
             retList.append((cmd, cmd))
         return retList
@@ -163,6 +142,7 @@ class Plugin(indigo.PluginBase):
         url = "http://{}/v2/devices/{}/actions/{}".format(host, dev.pluginProps["address"], command)
         self.logger.debug(u"{}: doDeviceCommand, url = {}, payload = {}".format(dev.name, url, payload))
         requests.put(url, headers=header, json=payload)
+        dev.updateStateOnServer(key="last_command", value=command)
     
     ########################################
     # Plugin Actions object callbacks (pluginAction is an Indigo plugin action instance)    def startRaising(self, pluginAction, dev):
@@ -175,50 +155,3 @@ class Plugin(indigo.PluginBase):
             self.doDeviceCommand(dev, pluginAction.props["command"], {"argument" : argument})
         else:
             self.doDeviceCommand(dev, pluginAction.props["command"])
-
-
-    ########################################
-    # Relay / Dimmer / Shade Action callback
-    ########################################
-    def actionControlDevice(self, action, dev):
-
-        ###### TURN ON ######
-        if action.deviceAction == indigo.kDeviceAction.TurnOn:
-            self.doDeviceCommand(dev, dev.pluginProps["on_cmd"])
-            dev.updateStateOnServer("onOffState", True)
-            
-        ###### TURN OFF ######
-        elif action.deviceAction == indigo.kDeviceAction.TurnOff:
-            self.doDeviceCommand(dev, dev.pluginProps["off_cmd"])
-            dev.updateStateOnServer("onOffState", False)
-
-    ######################
-    # Fan Action callback
-    ######################
-    def actionControlSpeedControl(self, action, dev):
-        
-        ###### TURN ON ######
-        if action.speedControlAction == indigo.kSpeedControlAction.TurnOn:
-            self.doDeviceCommand(dev, dev.pluginProps["on_cmd"])
-            dev.updateStateOnServer("onOffState", True)
-
-        ###### TURN OFF ######
-        elif action.speedControlAction == indigo.kSpeedControlAction.TurnOff:
-            self.doDeviceCommand(dev, dev.pluginProps["off_cmd"])
-            dev.updateStateOnServer("onOffState", False)
-            
-        ###### SET SPEED INDEX ######
-        elif action.speedControlAction == indigo.kSpeedControlAction.SetSpeedIndex:
-            self.logger.debug(u"{}: SetSpeedIndex to {}".format(dev.name, action.actionValue))
-
-        ###### SET SPEED LEVEL ######
-        elif action.speedControlAction == indigo.kSpeedControlAction.SetSpeedLevel:
-            self.logger.debug(u"{}: SetSpeedLevel to {}".format(dev.name, action.actionValue))
-
-        ###### INCREASE SPEED INDEX BY ######
-        elif action.speedControlAction == indigo.kSpeedControlAction.IncreaseSpeedIndex:
-            self.logger.debug(u"{}: IncreaseSpeedIndex by {}".format(dev.name, action.actionValue))
-
-        ###### DECREASE SPEED INDEX BY ######
-        elif action.speedControlAction == indigo.kSpeedControlAction.DecreaseSpeedIndex:
-            self.logger.debug(u"{}: DecreaseSpeedIndex by {}".format(dev.name, action.actionValue))
