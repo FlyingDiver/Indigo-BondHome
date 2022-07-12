35046693c6517f2f0a709690a08c16533aed379f
Joe Keenan <joe@flyingdiver.com>
2022-07-12 14:35:43 -0400

    clean up redundant exception handling

--- BondHome.indigoPlugin/Contents/Server Plugin/bondhome.py
+++ BondHome.indigoPlugin/Contents/Server Plugin/bondhome.py
@@ -17,7 +17,6 @@ class BondHome(object):
 
     def __init__(self, address, token):
         self.logger = logging.getLogger("Plugin.BondHome")
-
         self.address = address
         self.token_header = {'BOND-Token': token}
         self.bridge_data = {}
@@ -41,13 +40,9 @@ class BondHome(object):
     def udp_start(self, callback):
         self.callback = callback
         if not self.sock:
-            try:
-                self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-                self.sock.settimeout(PING_TIMEOUT)
-            except (Exception,):
-                raise
-            else:
-                self.logger.debug(u"udp_start() socket listener started")
+            self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            self.sock.settimeout(PING_TIMEOUT)
+            self.logger.debug(u"udp_start() socket listener started")
 
         # start up the receiver thread
         self.receive_thread.start()
@@ -65,22 +60,18 @@ class BondHome(object):
                 continue
             except socket.error as err:
                 raise
-            else:
-                try:
-                    data = json.loads(json_data.decode("utf-8"))
-                except Exception as err:
-                    raise
 
-                # don't send ping ack
-                if len(data) == 1:
-                    continue
+            data = json.loads(json_data.decode("utf-8"))
 
-                # fix up the data
+            # ignore ping messages
+            if len(data) == 1:
+                continue
 
-                topic = data['t'].split('/')
-                data['id'] = topic[1]
+            # fix up the data
+            topic = data['t'].split('/')
+            data['id'] = topic[1]
 
-                self.callback(data)
+            self.callback(data)
 
     ########################################
     # Commands to the Bridge
@@ -91,48 +82,37 @@ class BondHome(object):
             payload = {}
         url = f"http://{self.address}/v2/devices/{device_id}/actions/{action}"
         self.logger.debug(f"device_action, url = {url}, payload = {payload}")
-        try:
-            resp = requests.put(url, headers=self.token_header, json=payload)
-            resp.raise_for_status()
-        except (Exception,):
-            raise
+        resp = requests.put(url, headers=self.token_header, json=payload)
+        resp.raise_for_status()
         self.logger.debug(f"device_action, resp = {resp}")
         return resp
 
     def get_bridge_version(self):
+        self.logger.debug(f"get_bridge_version")
         url = f"http://{self.address}/v2/sys/version"
-        try:
-            resp = requests.get(url, headers=self.token_header)
-            resp.raise_for_status()
-        except (Exception,):
-            raise
+        resp = requests.get(url, headers=self.token_header)
+        resp.raise_for_status()
         return resp.json()
 
     def get_bridge_info(self):
+        self.logger.debug(f"get_bridge_info")
         url = f"http://{self.address}/v2/bridge"
-        try:
-            resp = requests.get(url, headers=self.token_header)
-            resp.raise_for_status()
-        except (Exception,):
-            raise
+        resp = requests.get(url, headers=self.token_header)
+        resp.raise_for_status()
         return resp.json()
 
     def set_bridge_info(self, data):
+        self.logger.debug(f"set_bridge_info = {data}")
         url = f"http://{self.address}/v2/bridge"
-        try:
-            resp = requests.patch(url, headers=self.token_header, json=data)
-            resp.raise_for_status()
-        except (Exception,):
-            raise
+        resp = requests.patch(url, headers=self.token_header, json=data)
+        resp.raise_for_status()
         return resp.json()
 
     def get_device_list(self):
+        self.logger.debug(f"get_device_list")
         url = f"http://{self.address}/v2/devices"
-        try:
-            resp = requests.get(url, headers=self.token_header)
-            resp.raise_for_status()
-        except (Exception,):
-            raise
+        resp = requests.get(url, headers=self.token_header)
+        resp.raise_for_status()
         self.logger.debug(f"get_device_list: {resp.json()}")
 
         retList = []
@@ -142,39 +122,31 @@ class BondHome(object):
         return retList
 
     def get_device(self, device_id):
+        self.logger.debug(f"get_device: {device_id}")
         url = f"http://{self.address}/v2/devices/{device_id}"
-        try:
-            resp = requests.get(url, headers=self.token_header)
-            resp.raise_for_status()
-        except (Exception,):
-            raise
+        resp = requests.get(url, headers=self.token_header)
+        resp.raise_for_status()
         return resp.json()
 
     def get_device_state(self, device_id):
+        self.logger.debug(f"get_device_state: {device_id}")
         url = f"http://{self.address}/v2/devices/{device_id}/state"
-        try:
-            resp = requests.get(url, headers=self.token_header)
-            resp.raise_for_status()
-        except (Exception,):
-            raise
+        resp = requests.get(url, headers=self.token_header)
+        resp.raise_for_status()
         return resp.json()
 
     def update_device_state(self, device_id, payload):
+        self.logger.debug(f"update_device_state: {device_id}, {payload}")
         url = f"http://{self.address}/v2/devices/{device_id}/state"
-        try:
-            resp = requests.patch(url, headers=self.token_header, json=payload)
-            resp.raise_for_status()
-        except (Exception,):
-            raise
+        resp = requests.patch(url, headers=self.token_header, json=payload)
+        resp.raise_for_status()
         return resp.json()
 
     def get_device_command_list(self, device_id):
+        self.logger.debug(f"get_device_command_list: {device_id}")
         url = f"http://{self.address}/v2/devices/{device_id}/commands"
-        try:
-            resp = requests.get(url, headers=self.token_header)
-            resp.raise_for_status()
-        except (Exception,):
-            raise
+        resp = requests.get(url, headers=self.token_header)
+        resp.raise_for_status()
         retList = []
         for key in resp.json():
             if not key.startswith("_"):     # skip internal keys
@@ -182,19 +154,15 @@ class BondHome(object):
         return retList
 
     def get_device_command(self, device_id, command_id):
+        self.logger.debug(f"get_device_command: {device_id}, {command_id}")
         url = "http://{}/v2/devices/{}/commands/{}".format(self.address, device_id, command_id)
-        try:
-            resp = requests.get(url, headers=self.token_header)
-            resp.raise_for_status()
-        except (Exception,):
-            raise
+        resp = requests.get(url, headers=self.token_header)
+        resp.raise_for_status()
         return resp.json()
 
     def set_device_command_signal(self, device_id, command_id, payload):
+        self.logger.debug(f"set_device_command_signal: {device_id}, {command_id}, {payload}")
         url = "http://{}/v2/devices/{}/commands/{}/signal".format(self.address, device_id, command_id)
-        try:
-            resp = requests.patch(url, headers=self.token_header, json=payload)
-            resp.raise_for_status()
-        except (Exception,):
-            raise
+        resp = requests.patch(url, headers=self.token_header, json=payload)
+        resp.raise_for_status()
         return resp.json()
